dcd3ace824ee2038f6142b6b62324121b711986a
Jade Manrique <jade.a.manrique@gmail.com>
2018-02-27 12:00:52 -0800

    added more files

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,7 +18,7 @@ var emails = [
 
 for (var i = 0; i < names.length; i++) {
 
-  var html = "<div><p>Name: " + names[i] + "</p>" + "<p>Email: " + emails[i] + "</p></div>";
+  var html = "<div class='contact'><p><span class='name'>Name:</span> " + names[i] + "</p>" + "<p><span class='email'>Email:</span> " + emails[i] + "</p></div>";
 
   document.getElementById("text").insertAdjacentHTML('beforeend', html);
 }
